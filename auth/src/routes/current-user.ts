d6a9001803f0d5b55f225a294a87471080e04399
ishmam02 <143619983+tuo47952@users.noreply.github.com>
2023-09-21 21:48:00 -0400

    Auth service init setup

---
+++ auth/src/routes/current-user.ts
@@ -0,0 +1,10 @@
+import express, { Request, Response } from 'express';
+
+
+const router = express.Router();
+
+router.get('/api/users/currentuser', (req, res) => {
+  res.send('Hi there!');
+});
+
+export { router as currentUserRouter };
